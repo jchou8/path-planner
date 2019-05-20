7e32cd626c9bb8ef2bbd29d42db4954ada9c2b4b
jchou8 <joseph98c@gmail.com>
2019-05-19 22:53:55 -0700

    set up express and routes

---
+++ server/handlers.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let map = []
+
+function createMap(req, res, next) {
+    res.status(501).send('Not yet implemented')
+}
+
+function setStart(req, res, next) {
+    res.status(501).send('Not yet implemented')
+}
+
+function setGoal(req, res, next) {
+    res.status(501).send('Not yet implemented')
+}
+
+function setCosts(req, res, next) {
+    res.status(501).send('Not yet implemented')
+}
+
+function findPath(req, res, next) {
+    res.status(501).send('Not yet implemented')
+}
+
+module.exports = {
+    createMap,
+    setStart,
+    setGoal,
+    setCosts,
+    findPath
+}
--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
-console.log('Hello world!')
+'use strict';
+
+const express = require('express')
+const morgan = require('morgan')
+const handlers = require('./handlers.js')
+
+const app = express()
+const addr = process.env.ADDR || ':80'
+const [host, port] = addr.split(':')
+
+app.use(morgan('dev'))
+
+// Set up generic error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.set("Content-Type", "text/plain");
+    res.status(500).send(err.message);
+});
+
+app.post('/api/maps', handlers.createMap)
+app.post('/api/paths/start', handlers.setStart)
+app.post('/api/paths/goal', handlers.setGoal)
+app.post('/api/costs', handlers.setCosts)
+app.get('/api/paths', handlers.findPath)
+
+app.listen(port, host, () => {
+    console.log(`server is listening at http://${addr}...`);
+});
